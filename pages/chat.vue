0089f945fefb8f616a2a77f1c3d54a4b11802f68
TechFishe <82123830+TechFishe@users.noreply.github.com>
2024-01-25 12:07:00 -0600

    Update chat.vue

--- pages/chat.vue
+++ pages/chat.vue
@@ -106,7 +106,7 @@
             <div class="flex flex-grow"></div>
             <div class="mr-8 h-8 flex">
                 <input type="text" v-model="msg" placeholder="Enter message" class="w-full bg-shark-900 text-lg rounded-l-sm pl-2 h-8 placeholder:italic outline-0">
-                <button @keypress.enter="sendChat" @click="sendChat" class="bg-shark-900 rounded-r-sm pr-2 hover:text-aero-100 transition-colors duration-200 ease-out">
+                <button @keypress.enter="sendChat" @click="sendChat" :disabled="msg === ''" class="bg-shark-900 rounded-r-sm pr-2 text-snow/45 enabled:hover:text-aero-200 transition-colors duration-200 ease-out">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
                     </svg>
