6fa0c8ffd99ab638258fd6bfcc96cf9b3828422a
TechFishe <82123830+TechFishe@users.noreply.github.com>
2024-01-30 15:18:29 -0600

    Update chat.vue

--- pages/chat.vue
+++ pages/chat.vue
@@ -223,7 +223,14 @@
     <div class="flex h-fullscreen">
         <aside class="2xl:w-1/6 w-3/12 border-r-2 border-r-snow/45 flex h-fullscreen flex-col">
             <article class="h-2/3">
-                <h2 class="text-5xl font-mono font-semibold">Friends</h2>
+                <div class="flex items-center justify-between">
+                    <h2 class="text-5xl font-mono font-semibold">Friends</h2>
+                    <NuxtLink>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M18 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0ZM3 19.235v-.11a6.375 6.375 0 0 1 12.75 0v.109A12.318 12.318 0 0 1 9.374 21c-2.331 0-4.512-.645-6.374-1.766Z" />
+                        </svg>
+                    </NuxtLink>
+                </div>
                 <ul class="pl-2">
                     <li v-for="friend in friends" class="flex items-center border-b border-b-snow/25 pb-1.5 last:pb-0 last:border-b-transparent">
                         <img :src="friend.pfp" alt="Friend pfp" width="32px" height="32px" />
