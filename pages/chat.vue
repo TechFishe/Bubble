8538974c3b6607c4dbde42c5162351f367a3745d
TechFishe <jacobb2008@icloud.com>
2024-01-30 06:29:50 -0600

    Can now delete chats!

--- pages/chat.vue
+++ pages/chat.vue
@@ -148,6 +148,19 @@
         }
     }
 
+    
+    async function deleteChat(chatIn: Chat){
+        if(currentFriend.value.id != 0){
+            const { error } = await supabase.from("private_chats").delete().eq("id", chatIn.id);
+            if(error) throw error;
+            else getChats(false);
+        } else{
+            const { error } = await supabase.from("group_chats").delete().eq("id", chatIn.id);
+            if(error) throw error;
+            else getChats(true);
+        }
+    }
+
     onMounted(async () => {
         if (!user.value) return;
         
@@ -244,15 +257,27 @@
                 <h1 class="text-6xl ml-1 font-mono font-semibold bg-clip-text text-transparent bg-gradient-to-r from-snow to-65% to-aero-100 w-fit">{{ currentGroup.group_name }}</h1>
             </section>
             <ul id="chatbox" class="ml-2 pb-2 overflow-y-scroll max-h-chatView flex w-screen flex-col-reverse">
-                <li v-if="currentFriend.full_name != ''" v-for="chat in chats" class="flex space-x-2">
+                <li v-if="currentFriend.full_name != ''" v-for="chat in chats" class="flex space-x-2 max-w-[81%] group">
                     <p v-if="chat.sent_by === currentFriend.user_id"><span class="text-sky-300">{{ currentFriend.full_name }}</span>:</p>
                     <p v-else-if="chat.sent_by === customUser.user_id"><span class="text-aero-300">{{ customUser.full_name }}</span>:</p>
                     <p>{{ chat.msg }}</p>
+                    <div class="flex flex-grow"></div>
+                    <button @click="deleteChat(chat)" class="hover:text-red-400 hidden group-hover:flex transition-colors duration-200 ease-in">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                        </svg>
+                    </button>
                 </li>
-                <li v-else v-for="chat in chats" class="flex space-x-2">
+                <li v-else v-for="chat in chats" class="flex space-x-2 max-w-[81%] group">
                     <p v-if="chat.sent_by != customUser.user_id"><span class="text-sky-300">{{ checkId(chat.sent_by) }}</span>:</p>
                     <p v-else><span class="text-aero-300">{{ customUser.full_name }}</span>:</p>
                     <p>{{ chat.msg }}</p>
+                    <div class="flex flex-grow"></div>
+                    <button @click="deleteChat(chat)" class="hover:text-red-400 hidden group-hover:flex transition-colors duration-200 ease-in">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                        </svg>
+                    </button>
                 </li>
             </ul>
             <div class="flex flex-grow"></div>
