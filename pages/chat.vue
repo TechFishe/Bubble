d86b2600dbf0d43b554d1cf49330783e28a5baa9
TechFishe <82123830+TechFishe@users.noreply.github.com>
2024-01-31 08:23:11 -0600

    Update chat.vue

--- pages/chat.vue
+++ pages/chat.vue
@@ -268,7 +268,7 @@
                 <h1 class="text-6xl ml-1 font-mono font-semibold bg-clip-text text-transparent bg-gradient-to-r from-snow to-65% to-aero-100 w-fit">{{ currentGroup.group_name }}</h1>
             </section>
             <ul id="chatbox" class="ml-2 pb-2 overflow-y-scroll max-h-chatView flex w-screen flex-col-reverse">
-                <li v-if="currentFriend.full_name != ''" v-for="chat in chats" class="flex space-x-2 max-w-[61%] border border-transparent hover:border-snow">
+                <li v-if="currentFriend.full_name != ''" v-for="chat in chats" class="flex space-x-2 shadow-md shadow-transparent hover:shadow-snow-45">
                     <p v-if="chat.sent_by === currentFriend.user_id"><span class="text-sky-300">{{ currentFriend.full_name }}</span>:</p>
                     <p v-else-if="chat.sent_by === customUser.user_id"><span class="text-aero-300">{{ customUser.full_name }}</span>:</p>
                     <p>{{ chat.msg }}</p>
