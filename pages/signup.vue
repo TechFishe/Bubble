d6a5af2d05747f14b7f16bbe3cc02363b29035eb
TechFishe <82123830+TechFishe@users.noreply.github.com>
2024-02-01 13:08:49 -0600

    Update signup.vue

--- pages/signup.vue
+++ pages/signup.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
     useHead({
         title: "Bubble | Sign up"
-    })
+    });
     
     import { useAlertStore } from "#imports";
     import { createAvatar } from '@dicebear/core';
@@ -16,20 +16,26 @@
     const firstName = ref("");
     const lastName = ref("");
 
-    async function signUp(){
+    const username = ref("");
+    const age = ref(0);
+    
+    const screenTwo = ref(false);
+
+    async function nextPage(){
         if(pass.value.length < 6){
             alertStore.msg = "Your password must be at least 6 characters.";
             alertStore.type = "error";
             alertStore.timesShown++;
             return;
-        }
-        else if(pass.value != confirmPass.value){
+        } else if(pass.value != confirmPass.value){
             alertStore.msg = "Your passwords do not match.";
             alertStore.type = "error";
             alertStore.timesShown++;
             return;
-        }
+        } else screenTwo.value = true;
+    }
 
+    /* async function signUp(){
         const { data, error } = await supabase.auth.signUp({ email: email.value, password: pass.value });
         if(error) throw error;
         //@ts-expect-error
@@ -40,29 +46,41 @@
         let avatar = createAvatar(bottts, { size: 64, seed: uuid }).toDataUriSync();
         let fullName = firstName.value + " " + lastName.value;
         //@ts-expect-error
-        const { error } = await supabase.from('users').insert({ user_id: uuid, full_name: fullName, friends: [], pfp: avatar });
+        const { error } = await supabase.from('users').insert({ user_id: uuid, full_name: fullName, pfp: avatar });
         if(error) throw error;
         else navigateTo("/login");
-    }
+    } */
 </script>
 
 <template>
     <main class="flex w-screen h-fullscreen justify-center items-center">
         <div class="flex flex-col bg-shark-950 rounded-lg items-center w-1/3 px-2 py-2">
             <h1 class="text-7xl uppercase font-mono font-black bg-clip-text border-b-[3px] border-b-snow/15 px-4 text-transparent bg-gradient-to-r from-snow to-65% to-aero-100">Sign up</h1>
-            <section class="space-y-4 w-full flex flex-col items-center mt-4">
-                <input v-model="email" type="email" placeholder="Email" class="w-full bg-shark-900 text-lg rounded-sm pl-2 h-8 placeholder:italic outline-0" />
-                <input v-model="pass" type="password" placeholder="Password" class="w-full bg-shark-900 text-lg rounded-sm pl-2 h-8 placeholder:italic outline-0" />
-                <input v-model="confirmPass" type="password" placeholder="Confirm Password" class="w-full bg-shark-900 text-lg rounded-sm pl-2 h-8 placeholder:italic outline-0" />
-                <section class="flex space-x-2 justify-center w-full">
-                    <input v-model="firstName" type="text" placeholder="First Name" class="w-full bg-shark-900 text-lg rounded-sm pl-2 h-8 placeholder:italic outline-0" />
-                    <input v-model="lastName" type="text" placeholder="Last Name" class="w-full bg-shark-900 text-lg rounded-sm pl-2 h-8 placeholder:italic outline-0" />
+            <div v-if="!screenTwo">
+                <section class="space-y-4 w-full flex flex-col items-center mt-4">
+                    <input v-model="email" type="email" placeholder="Email" class="w-full bg-shark-900 text-lg rounded-sm pl-2 h-8 placeholder:italic outline-0" />
+                    <input v-model="pass" type="password" placeholder="Password" class="w-full bg-shark-900 text-lg rounded-sm pl-2 h-8 placeholder:italic outline-0" />
+                    <input v-model="confirmPass" type="password" placeholder="Confirm Password" class="w-full bg-shark-900 text-lg rounded-sm pl-2 h-8 placeholder:italic outline-0" />
+                    <section class="flex space-x-2 justify-center w-full">
+                        <input v-model="firstName" type="text" placeholder="First Name" class="w-full bg-shark-900 text-lg rounded-sm pl-2 h-8 placeholder:italic outline-0" />
+                        <input v-model="lastName" type="text" placeholder="Last Name" class="w-full bg-shark-900 text-lg rounded-sm pl-2 h-8 placeholder:italic outline-0" />
+                    </section>
                 </section>
-            </section>
-            <section class="space-x-4 w-full justify-center flex mt-4">
-                <button @click="signUp" :disabled="email === '' || pass === '' || confirmPass === '' || firstName === '' || lastName === ''" class="border border-snow/85 px-4 py-1 disabled:cursor-not-allowed disabled:opacity-60 rounded-md text-xl enabled:hover:text-aero-400 enabled:hover:border-aero-200/85 enabled:hover:drop-shadow-[0_4px_3px_rgba(158,252,207,0.1)] transition-all duration-200 ease-out">Sign up</button>
-                <NuxtLink to="/login" class="border border-snow/85 px-4 py-1 rounded-md text-xl hover:text-red-400 hover:border-red-200/85 hover:drop-shadow-[0_4px_3px_rgba(254,202,202,0.1)] transition-all duration-200 ease-out">Back</NuxtLink>
-            </section>
+                <section class="space-x-4 w-full justify-center flex mt-4">
+                    <button @click="signUp" :disabled="email === '' || pass === '' || confirmPass === '' || firstName === '' || lastName === ''" class="border border-snow/85 px-4 py-1 disabled:cursor-not-allowed disabled:opacity-60 rounded-md text-xl enabled:hover:text-aero-400 enabled:hover:border-aero-200/85 enabled:hover:drop-shadow-[0_4px_3px_rgba(158,252,207,0.1)] transition-all duration-200 ease-out">Next</button>
+                    <NuxtLink to="/login" class="border border-snow/85 px-4 py-1 rounded-md text-xl hover:text-red-400 hover:border-red-200/85 hover:drop-shadow-[0_4px_3px_rgba(254,202,202,0.1)] transition-all duration-200 ease-out">Back</NuxtLink>
+                </section>
+            </div>
+            <div v-else>
+                <section class="space-y-4 w-full flex flex-col items-center mt-4">
+                    <input v-model="username" type="text" placeholder="Username" class="w-full bg-shark-900 text-lg rounded-sm pl-2 h-8 placeholder:italic outline-0" />
+                    <input v-model="age" type="number" placeholder="Age" class="w-full bg-shark-900 text-lg rounded-sm pl-2 h-8 placeholder:italic outline-0" />
+                    <input type="file" accept="image/*" placeholder="Profile Pic" class="w-full bg-shark-900 text-lg rounded-sm pl-2 h-8 placeholder:italic outline-0" />
+                </section>
+                <section class="w-full justify-center flex mt-4">
+                    <button @click="signUp" :disabled="username === '' || age === 0" class="border border-snow/85 px-4 py-1 disabled:cursor-not-allowed disabled:opacity-60 rounded-md text-xl enabled:hover:text-aero-400 enabled:hover:border-aero-200/85 enabled:hover:drop-shadow-[0_4px_3px_rgba(158,252,207,0.1)] transition-all duration-200 ease-out">Sign up</button>
+                </section>
+            </div>
         </div>
     </main>
 </template>
