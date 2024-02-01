d714fda6a08c204e3e382a2e91d20e41d3e4f9c8
TechFishe <82123830+TechFishe@users.noreply.github.com>
2024-02-01 09:55:40 -0600

    Update chat.vue

--- pages/chat.vue
+++ pages/chat.vue
@@ -234,7 +234,7 @@
                 <ul class="pl-2 overflow-y-scroll">
                     <li v-for="friend in friends" class="flex items-center border-b border-b-snow/25 pb-1.5 last:pb-0 last:border-b-transparent">
                         <img :src="friend.pfp" alt="Friend pfp" width="32px" height="32px" />
-                        <span class="text-xl text-aero-100 flex flex-grow h-fit ml-1">{{ friend.full_name }}</span>
+                        <span class="text-xl flex flex-grow h-fit ml-1">{{ friend.full_name }}</span>
                         <button @click="setFriend(friend)" class="hover:text-aero-200 transition-colors duration-200 ease-in">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
@@ -255,7 +255,7 @@
                 <ul class="pl-2 overflow-y-scroll">
                     <li v-for="group in groups" class="flex items-center border-b border-b-snow/25 pb-1.5 last:pb-0 last:border-b-transparent">
                         <img :src="group.pfp" alt="Group pfp" width="24px" height="24px" />
-                        <span class="text-xl text-aero-100 flex flex-grow h-fit ml-1">{{ group.group_name }}</span>
+                        <span class="text-xl flex flex-grow h-fit ml-1">{{ group.group_name }}</span>
                         <button @click="setGroup(group)" class="hover:text-aero-200 transition-colors duration-200 ease-in">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
@@ -278,7 +278,7 @@
                 <li v-if="currentFriend.full_name != ''" v-for="chat in chats" class="flex space-x-2 hover:bg-shark-900 hover:cursor-default w-fit group rounded-md py-px px-2 transition-all duration-100 ease-out">
                     <p v-if="chat.sent_by === currentFriend.user_id"><span class="text-sky-300">{{ currentFriend.full_name }}</span>:</p>
                     <p v-else-if="chat.sent_by === customUser.user_id"><span class="text-aero-300">{{ customUser.full_name }}</span>:</p>
-                    <p>{{ chat.msg }}</p>
+                    <p class="text-wrap">{{ chat.msg }}</p>
                     <section class="hidden group-hover:flex">
                         <button @click="copyChat(chat)" class="hover:text-aero-200 transition-colors duration-200 ease-in">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
@@ -292,11 +292,10 @@
                         </button>
                     </section>
                 </li>
-                <li v-else v-for="chat in chats" class="flex space-x-2 max-w-[61%] group">
+                <li v-else v-for="chat in chats" class="flex space-x-2 hover:bg-shark-900 hover:cursor-default w-fit group rounded-md py-px px-2 transition-all duration-100 ease-out">
                     <p v-if="chat.sent_by != customUser.user_id"><span class="text-sky-300">{{ checkId(chat.sent_by) }}</span>:</p>
                     <p v-else><span class="text-aero-300">{{ customUser.full_name }}</span>:</p>
-                    <p>{{ chat.msg }}</p>
-                    <!-- <div class="flex flex-grow"></div>
+                    <p class="text-wrap">{{ chat.msg }}</p>
                     <section class="hidden group-hover:flex">
                         <button @click="copyChat(chat)" class="hover:text-aero-200 transition-colors duration-200 ease-in">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
@@ -308,7 +307,7 @@
                                 <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                             </svg>
                         </button>
-                    </section> -->
+                    </section>
                 </li>
             </ul>
             <div class="flex flex-grow"></div>
