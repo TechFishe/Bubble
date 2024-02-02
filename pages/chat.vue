b2d0d482159e53cf5f2002bfebd26ecc8727f852
TechFishe <82123830+TechFishe@users.noreply.github.com>
2024-02-02 09:15:11 -0600

    Update chat.vue

--- pages/chat.vue
+++ pages/chat.vue
@@ -134,7 +134,7 @@ import { compileScript } from 'vue/compiler-sfc';
                         </svg>
                     </NuxtLink>
                 </div>
-                <ul v-if="friendRequests.length !== 0" class="mx-1 -z-10 space-x-1 flex w-full overflow-x-scroll">
+                <ul v-if="friendRequests.length !== 0" class="px-1 space-x-1 flex w-full overflow-x-scroll">
                     <li v-for="request in friendRequests" class="bg-shark-900 rounded-md h-16 whitespace-nowrap w-full py-0.5 px-1 flex">
                         <div class="flex flex-col">
                             <section class="flex items-center">
