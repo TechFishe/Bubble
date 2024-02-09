e9264b46ae41ecee4433e0f506e5ce32874bfe45
TechFishe <82123830+TechFishe@users.noreply.github.com>
2024-02-09 10:05:16 -0600

    Update add-member.vue

--- pages/add-member.vue
+++ pages/add-member.vue
@@ -6,9 +6,25 @@
 
 <template>
     <main class="flex w-screen h-fullscreen justify-center items-center">
-        <div class="flex flex-col bg-shark-950 rounded-lg items-center w-1/3 px-2 py-2">
-            <h1 class="text-7xl uppercase font-mono font-black bg-clip-text border-b-[3px] border-b-snow/15 px-4 text-transparent bg-gradient-to-r from-snow to-65% to-aero-100">New group</h1>
-            <input v-model="name" type="text" placeholder="Group name" class="w-full bg-shark-900 text-lg mt-2 rounded-sm pl-2 h-8 placeholder:italic outline-0" />
+      <div class="flex flex-col bg-shark-950 rounded-lg items-center 2xl:w-1/2 md:w-2/3 px-2 py-2 space-y-2">
+            <h1 class="text-7xl uppercase font-mono font-black bg-clip-text border-b-[3px] border-b-snow/15 px-4 text-transparent bg-gradient-to-r from-snow to-65% to-aero-100">Add Member</h1>
+            <div class="bg-shark-900 rounded-sm h-8 flex space-x-1 w-1/2 items-center pr-2">
+                <input type="search" placeholder="Search" class="w-full bg-shark-900 text-lg pl-2 placeholder:italic outline-0" />
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+                </svg>
+            </div>
+            <ul class="w-full grid grid-cols-4 grid-rows-5">
+                <li v-for="user in list" class="flex items-center px-1 py-0.5 border-r border-r-snow/45 last:border-r-transparent">
+                    <img :src="user.pfp" alt="Friend pfp" width="32px" height="32px" />
+                    <span class="text-xl flex flex-grow h-fit ml-1">{{ user.username }}</span>
+                    <button @click="addMember(user)" class="hover:text-aero-200 transition-colors duration-200 ease-in">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                        </svg>
+                    </button>
+                </li>
+            </ul>
         </div>
     </main>
 </template>
