837ed0c689a8735c3d86e0802cfbe8ee6a1d4f91
TechFishe <82123830+TechFishe@users.noreply.github.com>
2024-02-09 11:28:28 -0600

    Update [id].vue

--- pages/chat/group/[id].vue
+++ pages/chat/group/[id].vue
@@ -119,8 +119,8 @@ s<script setup lang="ts">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-10 h-10">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M18 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0ZM3 19.235v-.11a6.375 6.375 0 0 1 12.75 0v.109A12.318 12.318 0 0 1 9.374 21c-2.331 0-4.512-.645-6.374-1.766Z" />
                         </svg>
-                    </NuxtLink> -->
-                </div>
+                    </NuxtLink>
+                </div> -->
             </section>
             <ul id="noScrollbar" class="ml-2 pb-2 overflow-y-scroll max-h-chatView flex w-screen flex-col-reverse">
                 <li v-for="chat in chats" class="flex space-x-2 hover:bg-shark-900 hover:cursor-default w-fit group rounded-md py-px px-2 transition-all duration-100 ease-out">
